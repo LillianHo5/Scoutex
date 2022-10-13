34104a19568a6066f91703190433a694aead66fa
LillianHo5 <92070679+LillianHo5@users.noreply.github.com>
2022-10-12 20:12:42 -0700

    Initial Commit

---
+++ Scoutex/ScoutexApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoutexApp.swift
+//  Scoutex
+//
+//  Created by Lillian Ho on 10/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ScoutexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
